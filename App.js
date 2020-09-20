db5d04d9256739ce2898f9b426919227a7053663
--global <--global>
2020-09-20 23:33:30 +0800

    movie list

--- App.js
+++ App.js
@@ -1,27 +1,174 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import {
-  View,
-  Text
-} from 'react-native';
-
-const App: () => React$Node = () => {
+import React, {useState, useEffect, useCallback} from 'react';
+import {View, Text, StyleSheet, Image, FlatList} from 'react-native';
+const MOCKED_MOVIES_DATA = [
+  {
+    id: '1',
+    title: '标题1',
+    year: '2015',
+    posters: {
+      thumbnail:
+        'http://resizing.flixster.com/DeLpPTAwX3O2LszOpeaMHjbzuAw=/53x77/dkpu1ddg7pbsk.cloudfront.net/movie/11/16/47/11164719_ori.jpg',
+    },
+  },
+  {
+    id: '2',
+    title: '标题2',
+    year: '2015',
+    posters: {
+      thumbnail:
+        'http://resizing.flixster.com/DeLpPTAwX3O2LszOpeaMHjbzuAw=/53x77/dkpu1ddg7pbsk.cloudfront.net/movie/11/16/47/11164719_ori.jpg',
+    },
+  },
+  {
+    id: '3',
+    title: '标题3',
+    year: '2015',
+    posters: {
+      thumbnail:
+        'http://resizing.flixster.com/DeLpPTAwX3O2LszOpeaMHjbzuAw=/53x77/dkpu1ddg7pbsk.cloudfront.net/movie/11/16/47/11164719_ori.jpg',
+    },
+  },
+  {
+    id: '4',
+    title: '标题4',
+    year: '2015',
+    posters: {
+      thumbnail:
+        'http://resizing.flixster.com/DeLpPTAwX3O2LszOpeaMHjbzuAw=/53x77/dkpu1ddg7pbsk.cloudfront.net/movie/11/16/47/11164719_ori.jpg',
+    },
+  },
+  {
+    id: '5',
+    title: '标题5',
+    year: '2015',
+    posters: {
+      thumbnail:
+        'http://resizing.flixster.com/DeLpPTAwX3O2LszOpeaMHjbzuAw=/53x77/dkpu1ddg7pbsk.cloudfront.net/movie/11/16/47/11164719_ori.jpg',
+    },
+  },
+  {
+    id: '6',
+    title: '标题6',
+    year: '2015',
+    posters: {
+      thumbnail:
+        'http://resizing.flixster.com/DeLpPTAwX3O2LszOpeaMHjbzuAw=/53x77/dkpu1ddg7pbsk.cloudfront.net/movie/11/16/47/11164719_ori.jpg',
+    },
+  },
+  {
+    id: '7',
+    title: '标题7',
+    year: '2015',
+    posters: {
+      thumbnail:
+        'http://resizing.flixster.com/DeLpPTAwX3O2LszOpeaMHjbzuAw=/53x77/dkpu1ddg7pbsk.cloudfront.net/movie/11/16/47/11164719_ori.jpg',
+    },
+  },
+  {
+    id: '8',
+    title: '标题8',
+    year: '2015',
+    posters: {
+      thumbnail:
+        'http://resizing.flixster.com/DeLpPTAwX3O2LszOpeaMHjbzuAw=/53x77/dkpu1ddg7pbsk.cloudfront.net/movie/11/16/47/11164719_ori.jpg',
+    },
+  },
+  {
+    id: '9',
+    title: '标题9',
+    year: '2015',
+    posters: {
+      thumbnail:
+        'http://resizing.flixster.com/DeLpPTAwX3O2LszOpeaMHjbzuAw=/53x77/dkpu1ddg7pbsk.cloudfront.net/movie/11/16/47/11164719_ori.jpg',
+    },
+  },
+  {
+    id: '10',
+    title: '标题10',
+    year: '2015',
+    posters: {
+      thumbnail:
+        'http://resizing.flixster.com/DeLpPTAwX3O2LszOpeaMHjbzuAw=/53x77/dkpu1ddg7pbsk.cloudfront.net/movie/11/16/47/11164719_ori.jpg',
+    },
+  },
+];
+// 在 组建中引入css类
+const App = () => {
+  const [state, setState] = useState({data: [], loaded: false});
+  const [url, setUrl] = useState(
+    'https://raw.githubusercontent.com/facebook/react-native/0.51-stable/docs/MoviesExample.json',
+  );
+  const fetchData = useCallback(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log(responseData);
+        // 注意，这里使用了this关键字，为了保证this在调用时仍然指向当前组件，我们需要对其进行“绑定”操作
+        setState((state) => ({
+          //把数据添加到 data 里（注意这里使用了数组的 concat 方法生成新数组，不能直接在原数组上 push！）：
+          data: state.data.concat(responseData.movies),
+          loaded: true,
+        }));
+      })
+      .catch((error) => {
+        console.log(error.message);
+      })
+      .done();
+  }, [url]);
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
   return (
-    <View style={{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center'
-    }}>
-        <Text>Hello world!</Text>
-    </View>
+    <FlatList
+      data={MOCKED_MOVIES_DATA}
+      renderItem={({item}) => (
+        <View style={styles.container}>
+          <Image
+            source={{uri: item.posters.thumbnail}}
+            style={styles.thumbnail}
+          />
+          <View style={styles.rightContainer}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Text style={styles.year}>{item.year}</Text>
+          </View>
+        </View>
+      )}
+      style={styles.list}
+      keyExtractor={(item) => item.id}
+    />
   );
 };
 
+// 创建一个css类
+const styles = StyleSheet.create({
+  list: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
+  },
+
+  container: {
+    padding: 10,
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+  rightContainer: {
+    flex: 1, //让rightContainer在父容器中占据 Image 之外剩下的全部空间
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
+  },
+});
 export default App;
