7a78060ea00c287c378a6d189104edeac9456f79
--global <--global>
2020-09-22 22:29:28 +0800

    add mobx

--- App.js
+++ App.js
@@ -4,39 +4,46 @@ import {View, Text, StyleSheet, Image, FlatList, Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createCompatNavigatorFactory} from '@react-navigation/compat';
+import {observable, actions} from 'mobx';
+import {Provider, observer} from 'mobx-react';
 import DetailsScreen from './app/views/DetailsScreen';
 import HomeScreen from './app/views/HomeScreen';
 global.XMLHttpRequest = global.originalXMLHttpRequest || global.XMLHttpRequest;
 // 在 组建中引入css类
 
-const Stack = createStackNavigator();
-export const App = () => (
+const Stack = createStackNavigator({});
+@observer
+class App extends React.Comment {
   // NavigationContainer 包含所有的元素，相当于 react-router 中最外层的Router
-  <NavigationContainer>
-    {
-      // Stack.Navigator 负责创建默认导航，相当于 react-router中的 Switch
-      // screenOptions 全局配置
-    }
-    <Stack.Navigator
-      initialRouteName="Home"
-      screenOptions={{
-        title: '测试题',
-        headerStyle: {
-          backgroundColor: '#f4511e',
-        },
-        headerTintColor: '#fff',
-        headerTitleStyle: {
-          fontWeight: 'bold',
-        },
-      }}>
-      {/* 相当于 react-router中的Route */}
-      <Stack.Screen
-        name="Details"
-        options={{title: <Text>1234</Text>, headerTitleAlign: 'center'}}
-        component={DetailsScreen}
-      />
-      <Stack.Screen name="Home" component={HomeScreen} />
-    </Stack.Navigator>
-  </NavigationContainer>
-);
+  render() {
+    return (
+      <NavigationContainer>
+        {
+          // Stack.Navigator 负责创建默认导航，相当于 react-router中的 Switch
+          // screenOptions 全局配置
+        }
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            title: '测试题',
+            headerStyle: {
+              backgroundColor: '#f4511e',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}>
+          {/* 相当于 react-router中的Route */}
+          <Stack.Screen
+            name="Details"
+            options={{title: <Text>1234</Text>, headerTitleAlign: 'center'}}
+            component={DetailsScreen}
+          />
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
 export default App;
